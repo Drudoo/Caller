9e11693a43b06f03039e09dbb8270dee8be73f1a
Drudoo <drudoo@drudoo.com>
2016-09-26 10:35:15 +0200

    Added Import and Main method

---
+++ Caller.java
@@ -0,0 +1,49 @@
+//For layout/GUI
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.JOptionPane;
+
+//For mouse+keyboard clicks
+import java.awt.event.*;
+
+//For clipboard (copy+paste)
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.ClipboardOwner;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+
+//For images/icons
+import javax.imageio.ImageIO;
+
+//For file usage
+import java.io.*;
+import java.io.File;
+import java.io.File;
+
+//For scroll panes
+import java.awt.Toolkit;
+
+//For Native look and feel
+//import com.sun.java.swing.plaf.gtk.GTKLookAndFeel;
+//import com.sun.java.swing.plaf.motif.MotifLookAndFeel;
+import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
+
+//For date, lists, arrays and other
+import java.util.*;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
+//For URL / URI
+import java.net.*;
+import java.net.URI;
+import java.net.URL;
+
+public class Caller extends JPanel implements ItemListener,ActionListener, MouseListener,ClipboardOwner {
+
+}
